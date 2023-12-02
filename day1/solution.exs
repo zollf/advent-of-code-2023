e7fe9abb99d17a4e6c8e2aad83e082f2b8dc595a
zollf <dylankennedy2000@hotmail.com>
2023-12-02 14:40:05 +0800

    Day 1

---
+++ day1/solution.exs
@@ -0,0 +1,26 @@
+defmodule AdventOfCode.Day1 do
+  def get_calibration(value) do
+    first_num_from_front = value |> first_num
+    first_num_from_back = value |> String.reverse() |> first_num
+    first_num_from_front * 10 + first_num_from_back
+  end
+
+  def first_num([head | tail]) do
+    case Integer.parse(head) do
+      {num, _rem} -> num
+      _ -> first_num(tail)
+    end
+  end
+
+  def first_num(value) when is_binary(value) do
+    value
+    |> String.split("")
+    |> first_num()
+  end
+end
+
+File.read!("day1/input.txt")
+|> String.split("\n")
+|> Enum.map(&AdventOfCode.Day1.get_calibration/1)
+|> Enum.sum()
+|> IO.inspect()
